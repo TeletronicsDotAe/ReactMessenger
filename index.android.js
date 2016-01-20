924b3718dd133acca16bc5156c15deeea59d8ff4
Kristian Frølund Jørgensen <kristian@tlt.local>
2016-01-20 10:09:33 +0400

    Begun android

--- index.android.js
+++ index.android.js
@@ -4,7 +4,9 @@
  */
 'use strict';
 
-var React = require('react-native');
+import React from 'react-native';
+import App from './lib/App';
+
 var {
   AppRegistry,
   StyleSheet,
@@ -12,41 +14,12 @@ var {
   View,
 } = React;
 
-var ReactMessenger = React.createClass({
-  render: function() {
+
+class ReactMessenger extends React.Component{
+  render(){
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <App />
     );
   }
-});
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
+}
 AppRegistry.registerComponent('ReactMessenger', () => ReactMessenger);
